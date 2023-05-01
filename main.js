a0b9796c1ba98088f65928fc903d754235e739f7
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2023-05-01 12:36:29 -0300

    feat: added loading message

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function getProjects(){
     const urlGitHub = 'https://api.github.com/users/Gutoneitzke/repos'
+    var loadingElement = document.getElementById('loading')
 
     fetch(urlGitHub,{
         method: 'GET',
@@ -8,6 +9,7 @@ function getProjects(){
         .then((response) => {
             console.log(response)
             showProjects(response)
+            loadingElement.style.display = 'none'
         })
         .catch((e) => {
             console.log(`Error -> ${e}`)
